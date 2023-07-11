51a810e50e6c2594c81896efea4b65ba63345b07
Hoon Wee <mrgravity817@gmail.com>
2023-07-11 09:27:17 +0900

    feat: supervisor to revive the completed process

--- lib/jobber.ex
+++ lib/jobber.ex
@@ -2,17 +2,10 @@ defmodule Jobber do
   @moduledoc """
   Documentation for `Jobber`.
   """
+  alias Jobber.Core.Job
+  alias Jobber.JobRunner
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Jobber.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_job(args) do
+    DynamicSupervisor.start_child(JobRunner, {Job, args})
   end
 end
